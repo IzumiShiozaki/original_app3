56d05563c0259a006e53a22952eefc59e999ebef
IzumiShiozaki <over.drive.0108@outlook.jp>
2019-05-13 23:49:11 +0900

    finish customized user-microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,33 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @article  = current_user.articles.build
+      @article = current_user.article.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
-  end
-
-  def help
-  end
-
-  def about
-  end
-
-  def contact
-  end
-
-  def japanese
-  end
-
-  def socialstudies
-  end
-
-  def math
-  end
-
-  def science
-  end
-
-  def english
   end
 
   def subject
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-
   root 'static_pages#home'
-  get '/help', to:'static_pages#help'
-  get '/about', to:'static_pages#about'
-  get '/contact', to:'static_pages#contact'
-  get '/japanese', to:'users#japanese'
-  get '/socialstudies', to:'users#socialstudies'
-  get '/math', to:'users#math'
-  get '/science', to: 'users#science'
-  get '/english', to: 'users#english'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
